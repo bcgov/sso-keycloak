5a29d3f179385479f5a158ebb11ada14717ca8d9
Junmin Ahn <junminahn@outlook.com>
2021-10-29 13:23:55 -0700

    chore(realm-profile-app): allow ssl connection in postgres

--- realm-profile-app/utils/db.ts
+++ realm-profile-app/utils/db.ts
@@ -1,19 +1,20 @@
-import { Pool } from 'pg';
+import { Pool, PoolConfig } from 'pg';
 import format from 'pg-format';
 import getConfig from 'next/config';
 
 const { serverRuntimeConfig = {} } = getConfig() || {};
-const { pg_host, pg_port, pg_user, pg_password, pg_database } = serverRuntimeConfig;
+const { pg_host, pg_port, pg_user, pg_password, pg_database, pg_ssl } = serverRuntimeConfig;
 let _pgPool: Pool | null = null;
 
-const pgConfig = {
+const pgConfig: PoolConfig = {
   host: pg_host,
   port: parseInt(pg_port),
   user: pg_user,
   password: pg_password,
   database: pg_database,
-  // ssl: { rejectUnauthorized: false },
 };
+
+if (pg_ssl) pgConfig.ssl = { rejectUnauthorized: false };
 
 _pgPool = new Pool(pgConfig);
 
