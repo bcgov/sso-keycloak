2894382b884c026c71a5a6f259b5aa97366513cb
Junmin Ahn <junminahn@outlook.com>
2021-11-29 16:56:09 -0800

    chore: remove realm registry app directory

--- realm-registry-app/layout/BCSans.tsx
+++
@@ -1,8 +0,0 @@
-import { TypographyStyle } from 'react-typography';
-import typography from '@button-inc/bcgov-theme/typography';
-import '@bcgov/bc-sans/css/BCSans.css';
-
-export default function BCSans() {
-  typography.options.baseFontSize = '16px';
-  return <TypographyStyle typography={typography} />;
-}
--- realm-registry-app/types/user-session.ts
+++
@@ -1,9 +0,0 @@
-export interface UserSession {
-  preferred_username: string;
-  given_name: string;
-  family_name: string;
-  email: string;
-  client_roles: string[];
-  idir_userid: string;
-  [key: string]: any;
-}
