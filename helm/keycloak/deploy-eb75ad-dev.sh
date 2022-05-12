0b8cb630680f3f2a021dad463700b8fcb1797f04
Junmin Ahn <junminahn@outlook.com>
2022-05-12 11:41:34 -0700

    chore: deploy keycloak with shared module

---
+++ helm/keycloak/deploy-eb75ad-dev.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+helm repo add sso-charts https://bcgov.github.io/sso-helm-charts
+helm repo update
+
+helm upgrade --install sso-keycloak sso-charts/sso-keycloak -n eb75ad-dev -f values-eb75ad-dev.yaml --version v1.2.3
