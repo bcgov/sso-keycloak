b39bcfd093da205d0075ff90a8b231139d81c267
Junmin Ahn <junminahn@outlook.com>
2022-05-16 11:13:05 -0700

    chore: deploy keycloak with a recommended config in gold

--- helm/keycloak/deploy-eb75ad-prod.sh
+++ helm/keycloak/deploy-eb75ad-prod.sh
@@ -3,4 +3,4 @@
 helm repo add sso-charts https://bcgov.github.io/sso-helm-charts
 helm repo update
 
-helm upgrade --install sso-keycloak sso-charts/sso-keycloak -n eb75ad-prod -f values-eb75ad-prod.yaml --version v1.2.3
+helm upgrade --install sso-keycloak sso-charts/sso-keycloak -n eb75ad-prod -f values-eb75ad-prod.yaml --version v1.3.0
