d1fff3c1fc2d102c10efd3aabbdeef4f5302daac
Junmin Ahn <junminahn@outlook.com>
2022-05-20 11:32:58 -0700

    chore: check kube endpoint before running helm charts

--- terraform/golddr/main.tf
+++ terraform/golddr/main.tf
@@ -1,3 +1,27 @@
+module "c6af30_dev" {
+  source  = "bcgov/openshift/deployer"
+  version = "0.5.0"
+
+  name      = "oc-deployer"
+  namespace = "c6af30-dev"
+}
+
+module "c6af30_test" {
+  source  = "bcgov/openshift/deployer"
+  version = "0.5.0"
+
+  name      = "oc-deployer"
+  namespace = "c6af30-test"
+}
+
+module "c6af30_prod" {
+  source  = "bcgov/openshift/deployer"
+  version = "0.5.0"
+
+  name      = "oc-deployer"
+  namespace = "c6af30-prod"
+}
+
 module "eb75ad_dev" {
   source  = "bcgov/openshift/deployer"
   version = "0.5.0"
@@ -20,6 +44,17 @@ module "eb75ad_prod" {
 
   name      = "oc-deployer"
   namespace = "eb75ad-prod"
+}
+
+output "c6af30_dev_secret" {
+  value = module.c6af30_dev.default_secret_name
+}
+
+output "c6af30_test_secret" {
+  value = module.c6af30_test.default_secret_name
+}
+output "c6af30_prod_secret" {
+  value = module.c6af30_prod.default_secret_name
 }
 
 output "eb75ad_dev_secret" {
