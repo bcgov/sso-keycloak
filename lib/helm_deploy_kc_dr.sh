95bb0ceda09d53ddb7f3da1b47e64071b676fac4
Jonathan Sharman <jon.w.sharman@gmail.com>
2022-06-06 16:51:08 -0700

    put correct deployment script

--- lib/helm_deploy_kc_dr.sh
+++ lib/helm_deploy_kc_dr.sh
@@ -13,3 +13,13 @@ cd helm/keycloak
 helm dep up
 
 echo "Made it here without crashing"
+# helm upgrade --install --atomic -f ./values.yaml "$@" digital-marketplace . \
+#   --debug --timeout=12m0s
+# helm upgrade --install  \
+#     "$@" \
+#     --debug --timeout=12m0s
+# helm upgrade --install "${NAME}" . \
+#     -n "${NAMESPACE}" \
+#     -f values.yaml \
+#     -f "values-${NAMESPACE}-${NAME}.yaml" \
+#     "$@"
