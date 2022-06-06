408ea1b91aa1e84f487acb18aaa70d157043bd3d
Jonathan Sharman <jon.w.sharman@gmail.com>
2022-06-06 14:37:17 -0700

    put bash script in a separate file

---
+++ lib/patroni_dr_to_active.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+echo "Setting patroni-dr to active"
+# TODO set namespace as a variable
+oc rsh -n c6af30-test sso-patroni-0
+curl -s -XPATCH -d '{  "standby_cluster":null}' http://localhost:8008/config | jq .
+exit
