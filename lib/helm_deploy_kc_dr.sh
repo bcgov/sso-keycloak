5f41fffb2d3c2b700659a0997f9a4579006b08c3
Jonathan Sharman <jon.w.sharman@gmail.com>
2022-06-06 16:48:56 -0700

    Start the keycloak deployment step

--- lib/helm_deploy_kc_dr.sh
+++ lib/helm_deploy_kc_dr.sh
@@ -13,13 +13,3 @@ cd helm/keycloak
 helm dep up
 
 echo "Made it here without crashing"
-# helm upgrade --install --atomic -f ./values.yaml "$@" digital-marketplace . \
-#   --debug --timeout=12m0s
-# helm upgrade --install  \
-#     "$@" \
-#     --debug --timeout=12m0s
-# helm upgrade --install "${NAME}" . \
-#     -n "${NAMESPACE}" \
-#     -f values.yaml \
-#     -f "values-${NAMESPACE}-${NAME}.yaml" \
-#     "$@"
