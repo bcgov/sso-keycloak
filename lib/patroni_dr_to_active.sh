afe2c46ff25e4a229b9bcb0fba80ab33475cfdae
Jonathan Sharman <jon.w.sharman@gmail.com>
2022-06-06 15:01:25 -0700

    print oc project

--- lib/patroni_dr_to_active.sh
+++ lib/patroni_dr_to_active.sh
@@ -4,6 +4,7 @@ set -euxo pipefail
 
 echo "Setting patroni-dr to active"
 # TODO set namespace as a variable
-oc rsh -n c6af30-test sso-patroni-0
-curl -s -XPATCH -d '{  "standby_cluster":null}' http://localhost:8008/config
+oc project
+# oc rsh -n c6af30-test sso-patroni-0
+# curl -s -XPATCH -d '{  "standby_cluster":null}' http://localhost:8008/config
 exit
