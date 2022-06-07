73a47b8a6fd350b2c105a1f0fb9ddd7442cad2c4
Jonathan Sharman <jon.w.sharman@gmail.com>
2022-06-07 12:50:30 -0700

    list files

--- helm/keycloak/deploy-gold-c6af30-test.sh
+++ helm/keycloak/deploy-gold-c6af30-test.sh
@@ -3,4 +3,6 @@
 helm repo add sso-charts https://bcgov.github.io/sso-helm-charts
 helm repo update
 
+ls
+
 helm upgrade --install sso-keycloak sso-charts/sso-keycloak -n c6af30-test -f values-gold-c6af30-test.yaml --version v1.2.1
