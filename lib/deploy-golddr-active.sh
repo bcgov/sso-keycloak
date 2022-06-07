fd2f7f8b6ef54df43ba014a0a39656ecf11eee11
Jonathan Sharman <jon.w.sharman@gmail.com>
2022-06-07 13:25:42 -0700

    Simplify the values file

--- lib/deploy-golddr-active.sh
+++ lib/deploy-golddr-active.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
+# Todo, make this general for all namespaces
 helm repo add sso-charts https://bcgov.github.io/sso-helm-charts
 helm repo update
 
-ls
+
 
 helm upgrade --install sso-keycloak sso-charts/sso-keycloak -n c6af30-test -f ./helm/keycloak/values-gold-c6af30-test.yaml --version v1.2.1
