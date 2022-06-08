700344ac4042447fbac99c1ef8854bcbd7cc4748
Jonathan Sharman <jon.w.sharman@gmail.com>
2022-06-07 21:11:51 -0700

    Reworked with new version

--- helm/keycloak/deploy-golddr-c6af30-test.sh
+++ helm/keycloak/deploy-golddr-c6af30-test.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+pwd="$(dirname "$0")"
+source "$pwd/../helpers.sh"
+
+if ! check_kube_context "api-golddr-devops-gov-bc-ca"; then
+    echo "invalid context"
+    exit 1
+fi
+
 helm repo add sso-charts https://bcgov.github.io/sso-helm-charts
 helm repo update
 
-helm upgrade --install sso-keycloak sso-charts/sso-keycloak -n c6af30-test -f values-c6af30-test.yaml --version v1.2.1
+helm upgrade --install sso-keycloak sso-charts/sso-keycloak -n c6af30-test -f values-golddr-c6af30-test.yaml --version v1.6.0
--- lib/deploy-golddr-active.sh
+++ lib/deploy-golddr-active.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 # Todo, make this general for all namespaces
+
+
+
+#!/bin/bash
+
+pwd="$(dirname "$0")"
+source "$pwd/../helpers.sh"
+
+if ! check_kube_context "api-golddr-devops-gov-bc-ca"; then
+    echo "invalid context"
+    exit 1
+fi
+
 helm repo add sso-charts https://bcgov.github.io/sso-helm-charts
 helm repo update
 
+helm upgrade --install sso-keycloak sso-charts/sso-keycloak -n c6af30-test -f ./helm/keycloak/values-golddr-c6af30-test.yaml --version v1.6.0
+
+# helm repo add sso-charts https://bcgov.github.io/sso-helm-charts
+# helm repo update
 
 
-helm upgrade --install sso-keycloak sso-charts/sso-keycloak -n c6af30-test -f ./helm/keycloak/values-gold-c6af30-test.yaml --version v1.2.1
+
+# helm upgrade --install sso-keycloak sso-charts/sso-keycloak -n c6af30-test -f ./helm/keycloak/values-gold-c6af30-test.yaml --version v1.2.1
