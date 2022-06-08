ce6357cafdf90055c4f1cecede86160e5f8a58d0
Jonathan Sharman <jon.w.sharman@gmail.com>
2022-06-08 14:30:08 -0700

    remove comments

--- helm/keycloak/transition-scripts/deploy-golddr-active.sh
+++ helm/keycloak/transition-scripts/deploy-golddr-active.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-# Todo, make this general for all namespaces
 
 NAMESPACE=$1
 
 pwd="$(dirname "$0")"
 source "./helm/helpers.sh"
-# TODO fix this context
-
-kubectl config current-context
 
 if ! check_kube_context "api-golddr-devops-gov-bc-ca"; then
     echo "invalid context"
@@ -22,10 +18,3 @@ cd ./helm/keycloak/
 helm upgrade --install sso-keycloak sso-charts/sso-keycloak \
  -n ${NAMESPACE} -f ./values-golddr-${NAMESPACE}.yaml  \
  -f ./transition-values/set-dr-to-active-${NAMESPACE}.yaml --version v1.6.0
-
-# helm repo add sso-charts https://bcgov.github.io/sso-helm-charts
-# helm repo update
-
-
-
-# helm upgrade --install sso-keycloak sso-charts/sso-keycloak -n ${NAMESPACE} -f ./helm/keycloak/values-gold-${NAMESPACE}.yaml --version v1.2.1
