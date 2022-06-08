0cd47d03032febe9e5f9e0d79f7cbafa041e3c0a
Jonathan Sharman <jon.w.sharman@gmail.com>
2022-06-08 14:26:31 -0700

    reinstate context chek

--- helm/keycloak/transition-scripts/deploy-golddr-active.sh
+++ helm/keycloak/transition-scripts/deploy-golddr-active.sh
@@ -6,10 +6,13 @@ NAMESPACE=$1
 pwd="$(dirname "$0")"
 source "./helm/helpers.sh"
 # TODO fix this context
-# if ! check_kube_context "api-golddr-devops-gov-bc-ca"; then
-#     echo "invalid context"
-#     exit 1
-# fi
+
+kubectl config current-context
+
+if ! check_kube_context "api-golddr-devops-gov-bc-ca"; then
+    echo "invalid context"
+    exit 1
+fi
 
 helm repo add sso-charts https://bcgov.github.io/sso-helm-charts
 helm repo update
