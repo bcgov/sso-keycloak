7e9d9b796a06fc72d3f7f6b63d2d2bd1ca5b6190
Jonathan Sharman <jon.w.sharman@gmail.com>
2022-06-08 13:25:02 -0700

    fix path

--- helm/keycloak/transition-scripts/deploy-golddr-active.sh
+++ helm/keycloak/transition-scripts/deploy-golddr-active.sh
@@ -4,7 +4,7 @@
 
 
 pwd="$(dirname "$0")"
-source "./helm/keycloak/helpers.sh"
+source "./helm/helpers.sh"
 # TODO fix this context
 # if ! check_kube_context "api-golddr-devops-gov-bc-ca"; then
 #     echo "invalid context"
