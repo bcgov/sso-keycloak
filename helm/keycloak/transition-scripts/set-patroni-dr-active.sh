06dbc5913b611a7b6fde45470d7c4c80ed6110a6
Jonathan Sharman <jon.w.sharman@gmail.com>
2022-06-13 17:14:30 -0700

    purposely set wrong config

--- helm/keycloak/transition-scripts/set-patroni-dr-active.sh
+++ helm/keycloak/transition-scripts/set-patroni-dr-active.sh
@@ -11,9 +11,5 @@ if ! check_kube_context "api-golddr-devops-gov-bc-ca"; then
     exit 1
 fi
 
-# helm repo add sso-charts https://bcgov.github.io/sso-helm-charts
-# helm repo update
-
-# cd ./keycloak/
-oc rsh -n ${NAMESPACE} sso-patroni-0 curl -s -XPATCH -d '{  "standby_cluster":null}' http://localhost:8008/config | jq .
+OUTPUT = oc rsh -n ${NAMESPACE} sso-patroni-0 curl -s -XPATCH -d '{"standby_cluster_blah":null}' http://localhost:8008/config | jq .
 
