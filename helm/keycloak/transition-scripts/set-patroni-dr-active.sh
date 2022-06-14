11c8ed8ea49453fc98051925210d904f321d3bc3
Jonathan Sharman <jon.w.sharman@gmail.com>
2022-06-13 18:22:09 -0700

    create the job

--- helm/keycloak/transition-scripts/set-patroni-dr-active.sh
+++ helm/keycloak/transition-scripts/set-patroni-dr-active.sh
@@ -11,9 +11,8 @@ if ! check_kube_context "api-golddr-devops-gov-bc-ca"; then
     exit 1
 fi
 
-OUTPUT=$(oc rsh -n ${NAMESPACE} sso-patroni-0 curl -sw '%{http_code}' -XPATCH -d '{"standby_cluster":null}' http://localhost:8008/config)
+OUTPUT=$(oc rsh -n ${NAMESPACE} sso-patroni-0 curl -s -o /dev/null -w "%{http_code}" -XPATCH -d '{"standby_cluster":null}' http://localhost:8008/config)
 
 echo ${OUTPUT}
 
-# curl -sw '%{http_code}' -o /dev/null -w "%{http_code}" http://localhost:8008/
 echo "::set-output name=patroniconfig::${OUTPUT}"
