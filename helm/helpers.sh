8a1b5bc17891f17a2febb361753f10474c58f3a3
Jonathan Sharman <jon.w.sharman@gmail.com>
2022-06-16 19:52:05 -0700

    Test the helpers

--- helm/helpers.sh
+++ helm/helpers.sh
@@ -3,8 +3,7 @@
 check_kube_context() {
   partial="$1"
   context=$(kubectl config current-context)
-  echo $partial
-  echo $context
+
   if [[ $context == *"$partial"* ]]; then
     return 0
   fi
