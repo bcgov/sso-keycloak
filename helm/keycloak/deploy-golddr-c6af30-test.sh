3633a3525e426c807e3361aeb360d795cdc554fc
Jonathan Sharman <jon.w.sharman@gmail.com>
2022-06-20 17:36:38 -0700

    feat: dr
    Create the 4 github actions

--- helm/keycloak/deploy-golddr-c6af30-test.sh
+++ helm/keycloak/deploy-golddr-c6af30-test.sh
@@ -11,4 +11,4 @@ fi
 helm repo add sso-charts https://bcgov.github.io/sso-helm-charts
 helm repo update
 
-helm upgrade --install sso-keycloak sso-charts/sso-keycloak -n c6af30-test -f values-golddr-c6af30-test.yaml --version v1.6.0
+helm upgrade --install sso-keycloak sso-charts/sso-keycloak -n c6af30-test -f values-golddr-c6af30-test.yaml --version v1.7.1
---
+++ helm/keycloak/transition-scripts/set-patroni-standby.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+NAMESPACE=$1
+# The port of the tsc service used to connect gold and golddr
+PORT=$2
+
+CLUSTER=$3
+
+echo "Setting patroni-"$CLUSTER" to standby"
+
+pwd="$(dirname "$0")"
+source "$pwd/../../helpers.sh"
+
+# Confirm this is run on $CLUSTER
+if ! check_kube_context "api-$CLUSTER-devops-gov-bc-ca"; then
+    echo "invalid context"
+    exit 1
+fi
+
+if [ $CLUSTER == golddr ]; then
+    HOST='"sso-patroni-gold.'${NAMESPACE}'.svc.cluster.local"'
+else
+    HOST='"sso-patroni-golddr.'${NAMESPACE}'.svc.cluster.local"'
+fi
+
+# Set the patroni config to be in standby mode
+RESPONSE=$(kubectl -n  ${NAMESPACE} exec sso-patroni-0 -- curl -s -w "%{http_code}" -XPATCH -d '{"standby_cluster": {"create_replica_methods": ["basebackup_fast_xlog"],"host": '$HOST',"port": '$PORT'}}' http://localhost:8008/config)
+
+RESPONSE_CODE=${RESPONSE: -3}
+CLUSTERCONFIG=${RESPONSE:0:-3}
+echo "The response code is: "$RESPONSE_CODE
+
+if [ $RESPONSE_CODE = 200 ]; then
+    echo "Patroni-$CLUSTER config response returned"
+else
+    echo "The $CLUSTER patroni instance did not return a 200 response"
+    exit 1
+fi
+
+STANDBY_CLUSTER_CONFIG_LENGTH=$(echo $CLUSTERCONFIG | jq .standby_cluster | jq length )
+echo "The length of the standby config json is: "$STANDBY_CLUSTER_CONFIG_LENGTH
+
+# The length of the standby config json is 0 when patroni is in active mode
+if [[ $STANDBY_CLUSTER_CONFIG_LENGTH == 0 ]]; then
+    echo "The patroni-$CLUSTER config in active mode"
+    exit 1
+else
+    echo "The patroni-$CLUSTER is now in standby mode"
+fi
