18ebcaa63c5e4bfc05b77056f7a6ab3d2e74faa4
Junmin Ahn <junminahn@outlook.com>
2022-06-27 17:12:44 -0700

    chore: remove sandbox keycloak values files

--- helm/keycloak/deploy-gold-c6af30-dev.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-helm repo add sso-charts https://bcgov.github.io/sso-helm-charts
-helm repo update
-
-helm upgrade --install sso-keycloak sso-charts/sso-keycloak -n c6af30-dev -f values-gold-c6af30-dev.yaml --version v1.7.1
