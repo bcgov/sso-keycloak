47bc8a40dc87069df5c9bf74052f9b05c91ac641
Junmin Ahn <junminahn@outlook.com>
2022-07-27 11:55:30 -0700

    feat: add custom github extention to allow a specific only

--- docker/keycloak/extensions-7.5-9/services/src/main/java/com/github/bcgov/keycloak/broker/oidc/CustomOIDCIdentityProviderFactory.java
+++ docker/keycloak/extensions-7.5-9/services/src/main/java/com/github/bcgov/keycloak/broker/oidc/CustomOIDCIdentityProviderFactory.java
@@ -1,25 +1,19 @@
 package com.github.bcgov.keycloak.broker.oidc;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Map;
 import org.keycloak.broker.oidc.OIDCIdentityProvider;
 import org.keycloak.broker.oidc.OIDCIdentityProviderConfig;
-import org.keycloak.broker.provider.AbstractIdentityProviderFactory;
+import org.keycloak.broker.oidc.OIDCIdentityProviderFactory;
 import org.keycloak.models.IdentityProviderModel;
 import org.keycloak.models.KeycloakSession;
-import org.keycloak.protocol.oidc.representations.OIDCConfigurationRepresentation;
-import org.keycloak.util.JsonSerialization;
 
 /** @author <a href="mailto:junmin@button.is">Junmin Ahn</a> */
-public class CustomOIDCIdentityProviderFactory
-    extends AbstractIdentityProviderFactory<OIDCIdentityProvider> {
+public class CustomOIDCIdentityProviderFactory extends OIDCIdentityProviderFactory {
 
-  public static final String PROVIDER_ID = "oidc";
+  public static final String PROVIDER_ID = "oidc-custom";
 
   @Override
   public String getName() {
-    return "OpenID Connect v1.0";
+    return "OpenID Connect v1.0 - Custom";
   }
 
   @Override
@@ -28,39 +22,7 @@ public class CustomOIDCIdentityProviderFactory
   }
 
   @Override
-  public OIDCIdentityProviderConfig createConfig() {
-    return new OIDCIdentityProviderConfig();
-  }
-
-  @Override
   public String getId() {
     return PROVIDER_ID;
-  }
-
-  @Override
-  public Map<String, String> parseConfig(KeycloakSession session, InputStream inputStream) {
-    return parseOIDCConfig(session, inputStream);
-  }
-
-  protected static Map<String, String> parseOIDCConfig(
-      KeycloakSession session, InputStream inputStream) {
-    OIDCConfigurationRepresentation rep;
-    try {
-      rep = JsonSerialization.readValue(inputStream, OIDCConfigurationRepresentation.class);
-    } catch (IOException e) {
-      throw new RuntimeException("failed to load openid connect metadata", e);
-    }
-    OIDCIdentityProviderConfig config = new OIDCIdentityProviderConfig();
-    config.setIssuer(rep.getIssuer());
-    config.setLogoutUrl(rep.getLogoutEndpoint());
-    config.setAuthorizationUrl(rep.getAuthorizationEndpoint());
-    config.setTokenUrl(rep.getTokenEndpoint());
-    config.setUserInfoUrl(rep.getUserinfoEndpoint());
-    if (rep.getJwksUri() != null) {
-      config.setValidateSignature(true);
-      config.setUseJwksUrl(true);
-      config.setJwksUrl(rep.getJwksUri());
-    }
-    return config.getConfig();
   }
 }
