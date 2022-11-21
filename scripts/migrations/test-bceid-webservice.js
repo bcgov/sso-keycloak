bbe68d5d72474b656039d4e8cc98f3f50f49cafd
Junmin Ahn <junminahn@outlook.com>
2022-11-21 12:01:04 -0800

    chore: update bceid web service helper function

--- scripts/migrations/test-bceid-webservice.js
+++ scripts/migrations/test-bceid-webservice.js
@@ -3,7 +3,7 @@ const { argv } = require('yargs');
 const Confirm = require('prompt-confirm');
 const { handleError, ignoreError } = require('../helpers');
 const { fetchBceidUser } = require('./helpers/migrate-target-bceidboth-users');
-const { type, search, env, auto } = argv;
+const { type, search, property, env, auto } = argv;
 
 async function main() {
   if (!env) {
@@ -14,7 +14,8 @@ async function main() {
       Flags:
       --env            BCeID Client environment; dev | test | prod
       --type           BCeID account type; Business | Individual
-      --search         BCeID account GUID to search for
+      --property       BCeID search property; userGuid | userId
+      --search         BCeID account search value
       --auto           Skips the confirmation before running the script
     `);
 
@@ -28,7 +29,7 @@ async function main() {
       if (!answer) return;
     }
 
-    const result = await fetchBceidUser({ accountType: type, matchKey: search, env });
+    const result = await fetchBceidUser({ accountType: type, property, matchKey: search, env });
     console.log('result', result);
 
     process.exit(0);
