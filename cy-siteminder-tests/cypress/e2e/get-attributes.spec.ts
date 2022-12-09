cca10be0b6976d2d775fc863e289898f7035480c
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2022-12-08 18:23:43 -0800

    chore: included bceid provider in the list

--- cy-siteminder-tests/cypress/e2e/get-attributes.spec.ts
+++ cy-siteminder-tests/cypress/e2e/get-attributes.spec.ts
@@ -4,7 +4,13 @@ describe('saml response attributes', () => {
   it('fetch attributes by cluster, idp and environment', () => {
     const clusters = ['SILVER', 'GOLD']
     const environments = ['DEV', 'TEST', 'PROD']
-    const idps = ['IDIR', 'BCEID_BASIC', 'BCEID_BUSINESS', 'BCEID_BASIC_BUSINESS']
+    const idps = [
+      'IDIR',
+      'BCEID',
+      'BCEID_BASIC',
+      'BCEID_BUSINESS',
+      'BCEID_BASIC_BUSINESS',
+    ]
 
     for (let x = 0; x < clusters.length; x++) {
       for (let y = 0; y < environments.length; y++) {
@@ -15,16 +21,28 @@ describe('saml response attributes', () => {
           bceid_basic_business_config,
         } = idp_config(clusters[x], environments[y])
         for (let z = 0; z < idps.length; z++) {
+          let idp = idps[z]
+          if (clusters[x] === 'GOLD' && idps[z] === 'BCEID') continue
+          if (['BCEID', 'BCEID_BASIC_BUSINESS'].includes(idps[z])) {
+            idp = 'BCEID_BUSINESS'
+          }
+          let url = fetchSsoUrl(clusters[x], environments[y], idps[z])
           cy.recordSamlAttributes(
-            fetchSsoUrl(clusters[x], environments[y], idps[z]),
-            eval(`${idps[z].toLowerCase()}_config.username`),
-            eval(`${idps[z].toLowerCase()}_config.password`),
+            url,
+            eval(`${idp.toLowerCase()}_config.username`),
+            eval(`${idp.toLowerCase()}_config.password`),
             idps[z]
           )
           cy.get('@samlattributekeys').then((data: any) => {
             cy.task(
               'log',
-              `${clusters[x]} - ${environments[y]} - ${idps[z]}: ${JSON.stringify(data)}`
+              `${JSON.stringify({
+                cluster: clusters[x],
+                environment: environments[y],
+                identityProvider: idps[z],
+                url,
+                attributes: data,
+              })}`
             )
           })
           cy.logout()
