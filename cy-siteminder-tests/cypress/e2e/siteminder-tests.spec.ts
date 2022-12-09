2917afa13734efecdf0946cffdca69013558cc1f
Nithin Shekar Kuruba <81444731+NithinKuruba@users.noreply.github.com>
2022-12-09 08:39:31 -0800

    chore: fix config (#265)

--- cy-siteminder-tests/cypress/e2e/siteminder-tests.spec.ts
+++ cy-siteminder-tests/cypress/e2e/siteminder-tests.spec.ts
@@ -73,27 +73,27 @@ describe('siteminder tests', () => {
   it('Basic/Business BCeID', () => {
     cy.testsite(
       fetchSsoUrl(ocp, env, 'BCEID_BASIC_BUSINESS'),
-      bceid_basic_business_config.username,
-      bceid_basic_business_config.password,
+      bceid_business_config.username,
+      bceid_business_config.password,
       'BCEID_BASIC_BUSINESS'
     )
     cy.get('@samlattributes').then((data: any) => {
       assert.deepEqual(
         data.guid,
-        bceid_basic_business_config.user_identifier,
+        bceid_business_config.user_identifier,
         'user_identifier'
       )
       assert.deepEqual(
         data.display_name,
-        bceid_basic_business_config.display_name,
+        bceid_business_config.display_name,
         'display_name'
       )
-      assert.deepEqual(data.username, bceid_basic_business_config.username, 'username')
-      assert.deepEqual(data.email, bceid_basic_business_config.email, 'email')
-      assert.equal(data.business_guid, bceid_basic_business_config.guid, 'business guid')
+      assert.deepEqual(data.username, bceid_business_config.username, 'username')
+      assert.deepEqual(data.email, bceid_business_config.email, 'email')
+      assert.equal(data.business_guid, bceid_business_config.guid, 'business guid')
       assert.deepEqual(
         data.business_legalname,
-        bceid_basic_business_config.legalname,
+        bceid_business_config.legalname,
         'business legalname'
       )
     })
