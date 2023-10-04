1a678b4326a9f0c1e33d7e8f4c655797f4d92a3f
Jon Langlois <jonathan-langlois@live.ca>
2023-10-04 12:54:21 -0700

    feat/k6-tests
    refactor old tests, include introspection, user info, and offline sessions

---
+++ k6/tokenIntrospection.js
@@ -0,0 +1,37 @@
+import { sleep } from 'k6';
+import { hitIntrospectionRoute, getAccessToken } from './helpers.js';
+import { username, password, clientId } from './env.js';
+
+const CONCURRENT_LOOPS = 1;
+const ITERATIONS_PER_LOOP = 1000;
+const LOOP_DELAY = 0.01;
+
+export const options = {
+    scenarios: {
+        synchronousExecutions: {
+            executor: 'per-vu-iterations',
+            vus: CONCURRENT_LOOPS,
+            iterations: ITERATIONS_PER_LOOP,
+        }
+    },
+    thresholds: {
+        http_req_failed: [
+            {
+                threshold: `rate<${MAX_ALLOWED_FAILURE_RATE}`,
+                // Set true if you want to exit at this threshold. Can be useful for heavy tests where you want to save the poor server if its failing.
+                // abortOnFail: true,
+            },
+        ],
+    },
+};
+
+export function setup() {
+    const accessToken = getAccessToken({ username, password, clientId, confidential: true });
+    return accessToken;
+}
+
+// VU code
+export default function (accessToken) {
+    sleep(LOOP_DELAY)
+    hitIntrospectionRoute(accessToken, 'master')
+}
