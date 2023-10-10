00bc1fa6b3954622f25dc2e9160e1a42dfa75c62
Nithin Shekar Kuruba <81444731+NithinKuruba@users.noreply.github.com>
2023-10-10 13:12:43 -0700

    feat: custom messages for two login errors (#318)
    * feat: update differentUserAuthenticated message
    
    * feat: update differentUserAuthenticated message ver2
    
    * feat: update differentUserAuthenticated message ver3

--- docker/keycloak/extensions-7.6/themes/src/main/resources/theme/bcgov/login/resources/js/script-20221128.js
+++ docker/keycloak/extensions-7.6/themes/src/main/resources/theme/bcgov/login/resources/js/script-20221128.js
@@ -9,6 +9,41 @@ document.addEventListener('DOMContentLoaded', function (event) {
   document.getElementById('kc-page-title').innerHTML = titleContent;
 
   addTooltips();
+
+  if (titleContent === 'Login Error:') {
+    if (
+      document.getElementsByClassName('login-err-username')[0] &&
+      document.getElementsByClassName('instruction-link')[0]
+    ) {
+      const pageURLQueryParams = new URLSearchParams(window.location.search);
+
+      const pageURLQueryParamsObject = {};
+
+      for (const [key, value] of pageURLQueryParams) {
+        pageURLQueryParamsObject[key] = value;
+      }
+
+      document.getElementsByClassName(
+        'instruction-link',
+      )[0].href = `${window.location.protocol}//${window.location.host}/auth/realms/standard/login-actions/restart?client_id=${pageURLQueryParamsObject?.client_id}&tab_id=${pageURLQueryParamsObject?.tab_id}`;
+
+      // Replace the username with the identity provider alias
+      const usernameRegex = /<[\w\d]+@(\w+)>/g;
+
+      const loginErrorString = document.getElementsByClassName('login-err-username')[0].innerText;
+
+      const updatedDifferentUserAuthenticatedMessage = loginErrorString.replace(
+        usernameRegex,
+        (match, capturedGroup) => {
+          // If a match is found, replace it with the captured group
+          // Otherwise, return the original match
+          return capturedGroup || match;
+        },
+      );
+
+      document.getElementsByClassName('login-err-username')[0].innerText = updatedDifferentUserAuthenticatedMessage;
+    }
+  }
 });
 
 function updateFavIcon() {
