8b6e0fc53413b5a370d1b6f44bc474065de6d857
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2024-04-23 09:28:05 -0700

    feat: import base64 package

--- docker/keycloak/extensions-22/services/src/main/java/com/github/bcgov/keycloak/protocol/oidc/mappers/IDPUserinfoMapper.java
+++ docker/keycloak/extensions-22/services/src/main/java/com/github/bcgov/keycloak/protocol/oidc/mappers/IDPUserinfoMapper.java
@@ -16,6 +16,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Base64;
 
 /** @author <a href="mailto:junmin@button.is">Junmin Ahn</a> */
 public class IDPUserinfoMapper extends AbstractOIDCProtocolMapper
