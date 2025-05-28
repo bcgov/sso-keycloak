af904402c3ff1f206e4f10a3b051c66862199f32
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2025-05-28 15:01:52 -0700

    feat: run terraform in gh action

--- docker/otp-provider/terraform/config.tf
+++ docker/otp-provider/terraform/config.tf
@@ -12,11 +12,4 @@ terraform {
       version = ">= 3.7.2"
     }
   }
-
-  backend "s3" {
-    bucket         = "xgr00q-dev-sso-otp-provider"
-    key            = "sso-otp-provider.tfstate"
-    region         = "ca-central-1"
-    dynamodb_table = "xgr00q-dev-otp-state-locking"
-  }
 }
