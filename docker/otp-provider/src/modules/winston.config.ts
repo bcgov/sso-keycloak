0c6db30dd405067884cbf9737c8137ec088015bd
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2025-06-03 13:52:06 -0700

    feat: used winston splat util

--- docker/otp-provider/src/modules/winston.config.ts
+++ docker/otp-provider/src/modules/winston.config.ts
@@ -3,11 +3,17 @@ import { config } from '../config';
 
 const { LOG_LEVEL, NODE_ENV } = config;
 
-const { combine, timestamp, json, errors } = winston.format;
+const { combine, timestamp, json, errors, splat } = winston.format;
 
 const logger = winston.createLogger({
   level: LOG_LEVEL,
-  format: combine(timestamp(), errors({ stack: NODE_ENV !== 'production' }), json()),
+  format: combine(
+    timestamp(),
+    errors({ stack: NODE_ENV !== 'production' }),
+    json(),
+    splat(),
+    NODE_ENV === 'production' ? winston.format.uncolorize() : winston.format.colorize(),
+  ),
   transports: [new winston.transports.Console()],
 });
 
