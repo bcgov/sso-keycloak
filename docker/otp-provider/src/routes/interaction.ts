c78a4e7327465b1a35ba38e8e5fb0ac33aae01ac
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2025-06-11 21:35:50 -0700

    fix: log the error when occurs during interaction

--- docker/otp-provider/src/routes/interaction.ts
+++ docker/otp-provider/src/routes/interaction.ts
@@ -1,9 +1,9 @@
 import express, { NextFunction, Request, Response } from 'express';
 import Provider from 'oidc-provider';
 import { authorize, generateOtp, login, userConsent, abortLogin } from '../controllers/auth-controller';
-import { resendOtp, userResendOtpWaitTimeSeconds } from '../controllers/otp-controller';
 import { setNoCache } from '../utils/helpers';
 import { errors } from 'oidc-provider';
+import logger from '../modules/winston.config';
 
 export const oidcRouter = async (oidcProvider: Provider, optionalArgs: { cspNonce: string } = { cspNonce: '' }) => {
   const oidcRouter = express.Router();
@@ -14,6 +14,7 @@ export const oidcRouter = async (oidcProvider: Provider, optionalArgs: { cspNonc
   oidcRouter.post('/:uid/abort', await abortLogin(oidcProvider));
   oidcRouter.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     if (err) {
+      logger.error('OIDC interaction error:', err);
       let errorMessage = 'An unexpected error occurred';
       let errorStatus = 500;
       if (err instanceof errors.OIDCProviderError) {
