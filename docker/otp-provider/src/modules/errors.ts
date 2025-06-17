05129efc3af612ca46e33a38f7aa7458ecf31340
Nithin Shekar Kuruba <81444731+NithinKuruba@users.noreply.github.com>
2025-06-16 17:32:47 -0700

    fix: handle expired otps when used by user (#448)

--- docker/otp-provider/src/modules/errors.ts
+++ docker/otp-provider/src/modules/errors.ts
@@ -2,4 +2,5 @@ export const errors = {
   OTPS_LIMIT_REACHED: 'You have reached the maximum number of OTP requests for today. Please try again tomorrow.',
   EMAIL_REQUIRED: 'Email is required.',
   INVALID_EMAIL: 'Invalid email.',
+  EXPIRED_OTP_WITH_RESEND: 'Your OTP has expired. Please request a new one to continue.',
 };
