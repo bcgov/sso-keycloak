0501678afdc57de31eb7afec424647c278b4f7c9
jonathan langlois <jonathan.langlois@gov.bc.ca>
2025-07-15 09:23:46 -0700

    chore: ci server
    run server using playwright in ci

--- docker/otp-provider/playwright.config.ts
+++ docker/otp-provider/playwright.config.ts
@@ -76,9 +76,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'yarn dev',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'yarn test:server',
+    url: 'http://localhost:3000/auth',
+    reuseExistingServer: !process.env.CI,
+  },
 });
