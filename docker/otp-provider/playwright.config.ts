d665aef2095e8b347f3ab8d45769a7f9f34f493f
jonathan langlois <jonathan.langlois@gov.bc.ca>
2025-07-15 11:25:44 -0700

    chore: debug ci
    run ci pipeline for debugging

--- docker/otp-provider/playwright.config.ts
+++ docker/otp-provider/playwright.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 0 : 0,
   /* Opt out of parallel tests on CI. */
   workers:  1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -42,17 +42,17 @@ export default defineConfig({
       workers: 1,
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-      workers: 1,
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    //   workers: 1,
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-      workers: 1,
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    //   workers: 1,
+    // },
 
     /* Test against mobile viewports. */
     // {
