6265ab423bccc78dd28f3fd8ce7e66b62d465a29
jonathan langlois <jonathan.langlois@gov.bc.ca>
2025-07-15 12:50:34 -0700

    chore: debug ci
    run ci pipeline for debugging

--- docker/otp-provider/e2e/seed.sql
+++ docker/otp-provider/e2e/seed.sql
@@ -1,3 +1,5 @@
+DELETE FROM "ClientConfig";
+
 INSERT INTO "ClientConfig"("clientId", "clientSecret", "grantTypes", "redirectUris", "scope", "responseTypes", "clientUri", "allowedCorsOrigins", "postLogoutRedirectUris", "tokenEndpointAuthMethod")
     VALUES ('conf-client', 's3cr3t', '{authorization_code, refresh_token}', '{http://localhost:3001}', 'openid email', '{code}', 'http://localhost:3001', '{http://localhost:3001}', '{http://localhost:3001}', 'client_secret_post')
 ON CONFLICT
