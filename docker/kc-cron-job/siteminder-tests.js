f83837a1bee435cb00bab26f8881b1b6532744b0
Nithin Shekar Kuruba <81444731+NithinKuruba@users.noreply.github.com>
2025-07-23 12:46:30 -0700

    feat: improved error handling and updated helm values (#474)

--- docker/kc-cron-job/siteminder-tests.js
+++ docker/kc-cron-job/siteminder-tests.js
@@ -2,41 +2,62 @@ import { Octokit } from 'octokit';
 import { createAppAuth } from '@octokit/auth-app';
 import dotenv from 'dotenv';
 import fs from 'fs';
+import { sendRcNotification } from './helpers.js';
 
 dotenv.config();
 
 const privateKey = fs.readFileSync('./github-siteminder-tests-private-key.pem', 'utf8');
 
 async function runTests(octokit, environment) {
-  await octokit.rest.actions.createWorkflowDispatch({
-    owner: 'bcgov',
-    repo: 'sso-keycloak',
-    workflow_id: 'siteminder-tests.yml',
-    ref: 'dev',
-    inputs: {
-      environment,
-      cluster: 'GOLD'
-    }
-  });
+  try {
+    await octokit.rest.actions.createWorkflowDispatch({
+      owner: 'bcgov',
+      repo: 'sso-keycloak',
+      workflow_id: 'siteminder-tests.yml',
+      ref: 'dev',
+      inputs: {
+        environment,
+        cluster: 'GOLD'
+      }
+    });
+  } catch (err) {
+    console.error(err);
+    await sendRcNotification(
+      'siteminder-tests',
+      `**[${environment}]Failed to trigger siteminder tests** \n\n` + JSON.stringify(err),
+      true
+    );
+  }
 }
 
 async function main() {
-  const octokit = new Octokit({
-    authStrategy: createAppAuth,
-    auth: {
-      appId: process.env.GH_SITMINDER_TESTS_APP_ID,
-      privateKey,
-      installationId: process.env.GH_SITMINDER_TESTS_INSTALLATION_ID
+  try {
+    let octokit;
+    try {
+      octokit = new Octokit({
+        authStrategy: createAppAuth,
+        auth: {
+          appId: process.env.GH_SITMINDER_TESTS_APP_ID,
+          privateKey,
+          installationId: process.env.GH_SITMINDER_TESTS_INSTALLATION_ID
+        }
+      });
+
+      await octokit.rest.apps.getAuthenticated();
+    } catch (err) {
+      console.error(err);
+      throw new Error('Failed to authenticate with github application');
     }
-  });
 
-  await octokit.rest.apps.getAuthenticated();
+    await runTests(octokit, 'DEV');
 
-  await runTests(octokit, 'DEV');
+    await runTests(octokit, 'TEST');
 
-  await runTests(octokit, 'TEST');
-
-  if (new Date().getDay() === 0) await runTests(octokit, 'PROD');
+    if (new Date().getDay() === 0) await runTests(octokit, 'PROD');
+  } catch (err) {
+    console.error(err);
+    await sendRcNotification('siteminder-tests', `**${err}** \n\n` + JSON.stringify(err), true);
+  }
 }
 
 main();
