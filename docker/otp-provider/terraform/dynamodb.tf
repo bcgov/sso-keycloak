901f9355637da9a565c72a17a03518209ca682cc
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2025-11-27 15:07:20 -0800

    feat: remove terraform version v1

--- docker/otp-provider/terraform/dynamodb.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_dynamodb_table" "otp_state_locking" {
-  hash_key = "LockID"
-  name     = var.lock_table_name
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-  billing_mode = "PAY_PER_REQUEST"
-}
