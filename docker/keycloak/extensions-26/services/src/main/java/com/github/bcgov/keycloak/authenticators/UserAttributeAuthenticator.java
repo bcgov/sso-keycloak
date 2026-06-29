b076f543d33fba3700a8f4487d5468169fb4a05b
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2026-06-29 15:31:03 -0700

    fix: minor code issues

--- docker/keycloak/extensions-26/services/src/main/java/com/github/bcgov/keycloak/authenticators/UserAttributeAuthenticator.java
+++ docker/keycloak/extensions-26/services/src/main/java/com/github/bcgov/keycloak/authenticators/UserAttributeAuthenticator.java
@@ -36,26 +36,20 @@ public class UserAttributeAuthenticator implements Authenticator {
     }
 
     Map<String, String> config = authConfig.getConfig();
+
+    if (config == null) {
+      denyAccess(context, session, null);
+      return;
+    }
+
     String attributeKey = config.get(UserAttributeAuthenticatorFactory.ATTRIBUTE_KEY);
     String attributeValue = config.get(UserAttributeAuthenticatorFactory.ATTRIBUTE_VALUE);
     String errorUrl = config.get(UserAttributeAuthenticatorFactory.ERROR_URL);
 
-    if (authConfig == null) {
-      denyAccess(context, session, errorUrl);
-      return;
-    }
-
-    Map<String, String> config = authConfig.getConfig();
-
-    if (config == null) {
-      denyAccess(context, session, errorUrl);
-      return;
-    }
-
     UserModel user = session.getAuthenticatedUser();
 
     if (user == null || !isValidString(attributeKey) || !isValidString(attributeValue)) {
-      denyAccess(context, session, errorUrl);
+      denyAccess(context, session, null);
       return;
     }
 
