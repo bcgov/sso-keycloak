cc82a6283b74d470459dc12fd7cbb66ca9219475
Nithin Shekar Kuruba <NithinShekar.Kuruba@gov.bc.ca>
2026-09-08 16:02:04 -0700

    feat: custom oidc idp that sends client_id as kc_idp_hint

---
+++ docker/keycloak/extensions-26/services/src/main/java/com/github/bcgov/keycloak/broker/oidc/ClientIdKcHintOIDCIdentityProvider.java
@@ -0,0 +1,48 @@
+package com.github.bcgov.keycloak.broker.oidc;
+
+import org.jboss.logging.Logger;
+import org.keycloak.broker.oidc.OIDCIdentityProvider;
+import org.keycloak.broker.oidc.OIDCIdentityProviderConfig;
+import org.keycloak.broker.provider.AuthenticationRequest;
+import org.keycloak.constants.AdapterConstants;
+import org.keycloak.models.ClientModel;
+import org.keycloak.models.KeycloakSession;
+
+import jakarta.ws.rs.core.UriBuilder;
+
+/** OIDC Identity Provider that replaces the {@code kc_idp_hint} query parameter with the
+ *  initiating Keycloak client's client ID before sending the authorization request upstream,
+ *  but only when the incoming request was originally hinted for {@link #BCGOVIDIR_HINT}.
+ */
+public class ClientIdKcHintOIDCIdentityProvider extends OIDCIdentityProvider {
+
+  private static final Logger logger = Logger.getLogger(ClientIdKcHintOIDCIdentityProvider.class);
+
+  static final String KC_IDP_HINT_PARAM = "kc_idp_hint";
+  static final String BCGOVIDIR_HINT = "bcgovidir";
+
+  public ClientIdKcHintOIDCIdentityProvider(KeycloakSession session, OIDCIdentityProviderConfig config) {
+    super(session, config);
+  }
+
+  @Override
+  public UriBuilder createAuthorizationUrl(AuthenticationRequest request) {
+    UriBuilder ub = super.createAuthorizationUrl(request);
+
+    String originalHint = request.getUriInfo().getQueryParameters().getFirst(AdapterConstants.KC_IDP_HINT);
+    if (!BCGOVIDIR_HINT.equals(originalHint)) {
+      return ub;
+    }
+
+    ClientModel client = request.getAuthenticationSession() != null
+        ? request.getAuthenticationSession().getClient()
+        : null;
+    String clientId = client != null ? client.getClientId() : null;
+
+    if (clientId != null && !clientId.isBlank()) {
+      ub.replaceQueryParam(KC_IDP_HINT_PARAM, clientId);
+    }
+
+    return ub;
+  }
+}
