51587a4e4eef5b509d410d5cfbffa8ae8fe9f8e6
Nithin Shekar Kuruba <NithinShekar.Kuruba@gov.bc.ca>
2026-09-08 19:11:13 -0700

    feat: ensure the idp feature can be used only in standard realm

--- docker/keycloak/extensions-26/services/src/main/java/com/github/bcgov/keycloak/broker/oidc/ClientIdKcHintOIDCIdentityProvider.java
+++ docker/keycloak/extensions-26/services/src/main/java/com/github/bcgov/keycloak/broker/oidc/ClientIdKcHintOIDCIdentityProvider.java
@@ -12,8 +12,9 @@ import jakarta.ws.rs.core.UriBuilder;
 
 /** OIDC Identity Provider that replaces the {@code kc_idp_hint} query parameter with the
  *  initiating Keycloak client's client ID before sending the authorization request upstream,
- *  but only when the incoming request was originally hinted for {@link #BCGOVIDIR_HINT} and
- *  the client has {@link #BCGOVIDIR_HINT} assigned as a default scope.
+ *  but only when the realm is {@link #STANDARD_REALM}, the incoming request was originally
+ *  hinted for {@link #BCGOVIDIR_HINT}, and the client has {@link #BCGOVIDIR_HINT} assigned
+ *  as a default scope.
  */
 public class ClientIdKcHintOIDCIdentityProvider extends OIDCIdentityProvider {
 
@@ -21,6 +22,7 @@ public class ClientIdKcHintOIDCIdentityProvider extends OIDCIdentityProvider {
 
   static final String KC_IDP_HINT_PARAM = "kc_idp_hint";
   static final String BCGOVIDIR_HINT = "bcgovidir";
+  static final String STANDARD_REALM = "standard";
 
   public ClientIdKcHintOIDCIdentityProvider(KeycloakSession session, OIDCIdentityProviderConfig config) {
     super(session, config);
@@ -31,7 +33,7 @@ public class ClientIdKcHintOIDCIdentityProvider extends OIDCIdentityProvider {
     UriBuilder ub = super.createAuthorizationUrl(request);
 
     String originalHint = request.getUriInfo().getQueryParameters().getFirst(AdapterConstants.KC_IDP_HINT);
-    if (!BCGOVIDIR_HINT.equals(originalHint)) {
+    if (!STANDARD_REALM.equals(request.getRealm().getName()) || !BCGOVIDIR_HINT.equals(originalHint)) {
       return ub;
     }
 
